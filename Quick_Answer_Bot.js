c73b3a150b5e24b3a044d96fc2c10c13f8358ba4
Shoshone Arnold <42348679+shoshonearnold@users.noreply.github.com>
2019-04-05 21:57:42 -0400

    Add files via upload

--- Quick_Answer_Bot.js
+++ Quick_Answer_Bot.js
@@ -1,3 +1,3 @@
 function getAnswer() {
-	document.getElementById("demo").innerHTML = "Hey! This is a work in progress."
+	document.getElementById("demo").innerHTML = "Hey! This is a work in progress, and as im writing this, im still a salesman at a logistics company, but thanks for checking out my work! I hope to get better soon.";
 }
