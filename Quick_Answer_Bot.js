78bd4298f76a9088c4a5bca94be684fe0514dc26
Shoshone Arnold <42348679+shoshonearnold@users.noreply.github.com>
2019-04-10 13:33:25 -0400

    Add files via upload

--- Quick_Answer_Bot.js
+++ Quick_Answer_Bot.js
@@ -1,3 +1,5 @@
-function getAnswer() {
-	document.getElementById("demo").innerHTML = "Hey! This is a work in progress, and as im writing this, im still a salesman at a logistics company, but thanks for checking out my work! I hope to get better soon.";
-}
+$(document).ready(function(){
+	$("#button").click(function(){
+			$("#answer").toggle();
+	});
+});
